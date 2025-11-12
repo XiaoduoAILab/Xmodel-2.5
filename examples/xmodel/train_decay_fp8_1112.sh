9d02b19fe3d678a74e5937ddc06fa113aae52d17
Yang Liu <liuyangfoam@xiaoduotech.com>
2025-11-12 12:29:02 +0800

    update

--- examples/xmodel/train_decay_fp8_1112.sh
+++ examples/xmodel/train_decay_fp8_1112.sh
@@ -88,6 +88,7 @@ TRAINING_ARGS=(
     --lr-wsd-decay-style exponential
     --min-lr 1.66667e-05  # decoupled-min-lr/(hidden_size/256)
     --decoupled-min-lr 1e-4
+    --lr-decay-iters 20000
     --lr-wsd-decay-iters 20000
     --lr-warmup-iters 0
     --bf16
