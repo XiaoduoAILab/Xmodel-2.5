90590cd9d5e431c331196ad9718445068fbeb628
Liu Yang <liuyangfoam@xiaoduotech.com>
2025-11-16 18:17:27 +0800

    Xmodel-2.5 checkpoints

--- experiments/upload/rename.py
+++ experiments/upload/rename.py
@@ -1,16 +1,15 @@
 import os
 import shutil
 
-# folder = r'D:\Users\Administrator\models\Xmodel-2-history\s1'
-# folder = "/data4/liuyang/Xmodel-2-history/s2"
-folder = "/data1/liuyang/Xmodel-2-history/decay"
+src_folder = "/data2/liuyang/i_line_ckpt/i_line_s1_fp8_0921/"
+dst_folder = "/data2/liuyang/Xmodel-2.5-history/"
 
-for sub in os.listdir(folder):
+for sub in os.listdir(src_folder):
     iter_num = sub.split("-")[1]
-    sub_path = os.path.join(folder, sub)
+    sub_path = os.path.join(src_folder, sub)
     # print(sub_path)
     src_path = os.path.join(sub_path, "pytorch_model.bin")
-    dst_path = os.path.join(folder, f"pytorch_model.{iter_num}")
+    dst_path = os.path.join(dst_folder, f"pytorch_model.{iter_num}")
     print(f'src_path: {src_path}')
     print(f'dst_path: {dst_path}')
     shutil.copy2(src_path, dst_path) 
