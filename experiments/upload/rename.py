56d5ca4c2e883b801fe91ed32c405384df8598d1
Liu Yang <liuyangfoam@xiaoduotech.com>
2025-11-21 09:12:56 +0800

    update

--- experiments/upload/rename.py
+++ experiments/upload/rename.py
@@ -1,16 +1,18 @@
 import os
 import shutil
+from tqdm import tqdm
 
 src_folder = "/data2/liuyang/i_line_ckpt/i_line_s1_fp8_0921-hf/"
 dst_folder = "/data2/liuyang/Xmodel-2.5-history/"
 
-for sub in os.listdir(src_folder):
+for sub in tqdm(os.listdir(src_folder)):
     iter_num = sub.split("_")[1]
     sub_path = os.path.join(src_folder, sub)
     # print(sub_path)
     src_path = os.path.join(sub_path, "pytorch_model.bin")
     dst_path = os.path.join(dst_folder, f"pytorch_model.{iter_num}")
-    print(f'src_path: {src_path}')
-    print(f'dst_path: {dst_path}')
+
     if os.path.isfile(src_path) and os.path.getsize(src_path) == 2700260395 and not os.path.isfile(dst_path): 
+        print(f'src_path: {src_path}')
+        print(f'dst_path: {dst_path}')
         shutil.copy2(src_path, dst_path) 
